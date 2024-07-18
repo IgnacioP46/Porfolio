c3609c99e8952ce3c8de158b6099832d1076dd41
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-07-18 18:51:04 +0200

    Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => `
     <div>
       ${Button("../assets/logo llamada.png", "662168668", "tel:662168668")}
       ${Button("../assets/logo GitHub.jfif", "GitHub", "https://github.com/dashboard")}
-      ${Button("../public/assets/logo Linkedin.jfif", "LinkedIn", "https://www.linkedin.com/in/ignacio-p%C3%A9rez-alvarez-77b10111b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app")}
+      ${Button("../public/assets/logo Linkedin.jfif", "LinkedIn", "https://www.linkedin.com/in/ignacio-perez-alvarez/")}
     </div>
   </footer>
 `;
